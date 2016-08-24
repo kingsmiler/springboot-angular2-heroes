4d17360baff814eb0b676dd7c8adbc616b1d15e8
kingsmiler <kingsmiler@163.com>
2016-08-24 11:16:49 +0800

    finished 5.service

---
+++ src/main/webapp/app/mock/mock-heroes.ts
@@ -0,0 +1,15 @@
+
+import {Hero} from "../models/hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name: '关羽'},
+    {id: 12, name: '张飞'},
+    {id: 13, name: '赵云'},
+    {id: 14, name: '黄忠'},
+    {id: 15, name: '马超'},
+    {id: 16, name: '张辽'},
+    {id: 17, name: '张郃'},
+    {id: 18, name: '庞德'},
+    {id: 19, name: '周瑜'},
+    {id: 20, name: '孙策'}
+];
