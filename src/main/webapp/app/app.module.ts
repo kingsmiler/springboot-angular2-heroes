78dec177bf68b7274f62191c3a0bb2b7c9ce2873
kingsmiler <kingsmiler@163.com>
2016-08-24 11:50:41 +0800

    6.route, step1, separate heroes component

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -2,6 +2,8 @@ import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
 import {FormsModule} from "@angular/forms";
 import {AppComponent} from "./app.component";
+import {HeroService} from "./services/hero.service";
+import {HeroesComponent} from "./components/heroes/heroes.component";
 import {HeroDetailComponent} from "./components/hero-detail/hero-detail.component";
 
 @NgModule({
@@ -11,11 +13,13 @@ import {HeroDetailComponent} from "./components/hero-detail/hero-detail.componen
     ],
     declarations: [
         AppComponent,
+        HeroesComponent,
         HeroDetailComponent
     ],
-    bootstrap: [
-        AppComponent
-    ]
+    providers: [
+        HeroService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
